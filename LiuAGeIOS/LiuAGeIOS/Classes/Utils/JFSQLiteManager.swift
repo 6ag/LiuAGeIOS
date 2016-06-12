5cb350ed70bb162b1d8463d892366572dac02b6e
zhoujianfeng <admin@6ag.cn>
2016-06-12 15:29:00 +0800

    导入第三方库

--- LiuAGeIOS/LiuAGeIOS/Classes/Utils/JFSQLiteManager.swift
+++ LiuAGeIOS/LiuAGeIOS/Classes/Utils/JFSQLiteManager.swift
@@ -7,7 +7,9 @@
 //
 
 import UIKit
+import FMDB
 
 class JFSQLiteManager: NSObject {
-
+    
+    
 }
--- LiuAGeIOS/Pods/Target Support Files/Pods-LiuAGeIOS/Pods-LiuAGeIOS-frameworks.sh
+++ LiuAGeIOS/Pods/Target Support Files/Pods-LiuAGeIOS/Pods-LiuAGeIOS-frameworks.sh
@@ -86,6 +86,7 @@ strip_invalid_archs() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CryptoSwift/CryptoSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FMDB/FMDB.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GRMustache.swift/Mustache.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
@@ -102,6 +103,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "$BUILT_PRODUCTS_DIR/Alamofire/Alamofire.framework"
   install_framework "$BUILT_PRODUCTS_DIR/CryptoSwift/CryptoSwift.framework"
+  install_framework "$BUILT_PRODUCTS_DIR/FMDB/FMDB.framework"
   install_framework "$BUILT_PRODUCTS_DIR/GRMustache.swift/Mustache.framework"
   install_framework "$BUILT_PRODUCTS_DIR/IQKeyboardManagerSwift/IQKeyboardManagerSwift.framework"
   install_framework "$BUILT_PRODUCTS_DIR/MJRefresh/MJRefresh.framework"
